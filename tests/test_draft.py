c72423d377fa59f394456c8b4d2c8b28a8850947
IreneGallini <igallini@macalester.edu>
2026-06-12 10:23:20 -0400

    imports + test_imports()
    make sure it's working with the right version of aimnet since it was recently updated

--- tests/test_draft.py
+++ tests/test_draft.py
@@ -5,9 +5,21 @@ A simple oxime (acetone oxime) known E/Z geometry
 RDKit's Chem.MolToSmiles
 did AIMNet2 optimization significantly move the geometry from where Auto3D left it or just fine tune it?
 '''
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-from aimnet.calculators import AIMNet2Calculator, AIMNet2ASE
-print("AIMNet2Calculator OK")
+import pytest
+from pathlib import Path
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+def test_imports():
+    """Sanity check: do the key dependencies import cleanly?"""
+    from Auto3D.auto3D import options, main
+    from aimnet.calculators import AIMNet2Calculator, AIMNet2ASE
+    assert True
 
 # RMSD testing built into RDKit
 from rdkit.Chem import AllChem
